a93ee1a60912a5d774dd3648a1e088db9729eb2f
Kürşat <draknes.gamin70@gmail.com>
2025-10-30 09:40:00 +0300

    feat: add product schema

--- src/lib/graphql/schema.ts
+++ src/lib/graphql/schema.ts
@@ -1,5 +1,3 @@
-import { productTypeDefs } from "./types/Product";
-
 // Base schema with Query and Mutation types
 const baseTypeDefs = `
   type Query {
@@ -26,7 +24,78 @@ const baseTypeDefs = `
   }
 `;
 
-export const typeDefs = [
-  baseTypeDefs,
-  productTypeDefs,
-];
+// GraphQL type definitions for Product
+const productTypeDefs = `
+  type Product {
+    id: ID!
+    name: String!
+    slug: String!
+    priceMinor: Int!
+    currency: String!
+    stockQty: Int!
+    createdAt: String!
+    updatedAt: String!
+    category: Category!
+  }
+
+  input CreateProductInput {
+    name: String!
+    slug: String!
+    priceMinor: Int!
+    currency: String!
+    stockQty: Int!
+    categoryId: ID!
+  }
+
+  input UpdateProductInput {
+    name: String
+    slug: String
+    priceMinor: Int
+    currency: String
+    stockQty: Int
+    categoryId: ID
+  }
+
+  input StockOperationInput {
+    qty: Int!
+  }
+
+  input PriceUpdateInput {
+    priceMinor: Int!
+  }
+
+  type ProductConnection {
+    products: [Product!]!
+    pagination: Pagination!
+  }
+
+  type StockCheckResult {
+    inStock: Boolean!
+    requiredQty: Int!
+  }
+
+  extend type Query {
+    adminProducts(
+      page: Int = 1
+      limit: Int = 10
+      search: String
+      categoryId: ID
+      inStockOnly: Boolean
+    ): ProductConnection!
+    adminProduct(id: ID!): Product
+    adminProductBySlug(slug: String!): Product
+    adminProductsByCategory(categoryId: ID!): [Product!]!
+    adminProductStockCheck(id: ID!, qty: Int = 1): StockCheckResult!
+  }
+
+  extend type Mutation {
+    adminCreateProduct(input: CreateProductInput!): Product!
+    adminUpdateProduct(id: ID!, input: UpdateProductInput!): Product!
+    adminDeleteProduct(id: ID!): Boolean!
+    adminIncreaseProductStock(id: ID!, input: StockOperationInput!): Product!
+    adminDecreaseProductStock(id: ID!, input: StockOperationInput!): Product!
+    adminUpdateProductPrice(id: ID!, input: PriceUpdateInput!): Product!
+  }
+`;
+
+export const typeDefs = [baseTypeDefs, productTypeDefs];
